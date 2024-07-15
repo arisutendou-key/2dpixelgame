c7f7a96004633ca0f23cdb6824460528054023ce
Anna <115884637+AClarke25@users.noreply.github.com>
2024-07-15 15:00:48 -0400

    adding basic player movements
    left right movement and jumping (although for now they can jump endlessly)

--- 2degame/Assets/Scenes/AnnaDev/Scripts/GameManager.cs
+++ 2degame/Assets/Scenes/AnnaDev/Scripts/GameManager.cs
@@ -6,19 +6,19 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public AudioSource mainBGMusic;
+
     // Start is called before the first frame update
     void Start()
     {
         if(SceneManager.GetActiveScene().buildIndex == 0){
             mainBGMusic.Play();
         }
-        mainBGMusic.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Return)){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
---
+++ 2degame/Assets/Scenes/AnnaDev/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private float horizontalInput;
+
+    public float moveSpeed = 10f;
+    public float jumpSpeed = 12f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // **** FOR LOW GRAVITY JUMPING FEEL:
+        // lower the player's gravity scale when in a specific scene
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        float nextVelocityX = horizontalInput * moveSpeed;
+
+        if(horizontalInput < 0){ // facing left
+            // Vector3 just sets all the corresponding dimensions
+            transform.localScale = new Vector3(-1, 1, 1);
+        } else if(horizontalInput > 0){ // facing right
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        float nextVelocityY = rb2d.velocity.y;
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            nextVelocityY = jumpSpeed;
+        }
+
+        // setting all the velocity to the rigidbody
+        rb2d.velocity = new Vector2(nextVelocityX, nextVelocityY);
+    }
+}
