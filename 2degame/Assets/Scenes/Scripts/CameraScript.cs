0c348adc2382fd029c0896d3affd45b14ad6c729
Anna <115884637+AClarke25@users.noreply.github.com>
2024-07-16 13:40:54 -0400

    player and camera fixes
    player is now frictionless and the camera follows the player's x and y, unless the player is at the bottom of the map, in which case it will only follow the player's x.

--- 2degame/Assets/Scenes/Scripts/CameraScript.cs
+++ 2degame/Assets/Scenes/Scripts/CameraScript.cs
@@ -8,6 +8,13 @@ public class CameraScript : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        // (add upper bound later)
+        if(player.transform.position.y < 0){
+            // follows the player's x, but keeps the camera's y and z as it moves 
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        } else {
+            // follows the player's x, but keeps the camera's y and z as it moves 
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        }
     }
 }
