1625b43bd18d2227ea0164f20d6175d731a32074
janjan1512 <janicevivienlau1512@gmail.com>
2024-07-17 15:15:35 -0400

    did background for venus and neptune

--- 2degame/Assets/Scenes/Scripts/Background.cs
+++ 2degame/Assets/Scenes/Scripts/Background.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        repeatWidth = collider.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Camera.main.transform.position.x > transform.position.x + repeatWidth) // in the right of the centerpiece
+        {
+            // teleport it forward
+            transform.position = new Vector3(transform.position.x + repeatWidth, transform.position.y, transform.position.z);
+        } 
+        if(Camera.main.transform.position.x < transform.position.x - repeatWidth) // in the left of the centerpiece
+        {
+            // teleport it backwards
+            transform.position = new Vector3(transform.position.x - repeatWidth, transform.position.y, transform.position.z);
+        }
+
+    }
 }
