b1a2a29146ef73d01dbd03f7f1a5e7da2a9707df
Anna <115884637+AClarke25@users.noreply.github.com>
2024-07-17 10:58:49 -0400

    star functionality and some ui stuff

--- 2degame/Assets/Scenes/AnnaDev/Scripts/PlayerController.cs
+++ 2degame/Assets/Scenes/AnnaDev/Scripts/PlayerController.cs
@@ -9,11 +9,17 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed = 10f;
     public float jumpSpeed = 12f;
+    public GameObject[] totalStars;
+    public float starsCollected = 0;
+    public float starPercentage;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = new Vector2(0, 0);
+        totalStars = GameObject.FindGameObjectsWithTag("star");
     }
 
     // Update is called once per frame
@@ -40,5 +46,27 @@ public class PlayerController : MonoBehaviour
 
         // setting all the velocity to the rigidbody
         rb2d.velocity = new Vector2(nextVelocityX, nextVelocityY);
+
+        print("All stars: " + totalStars.Length);
+        print("Stars collected: " + starsCollected);
+        starPercentage = (starsCollected / totalStars.Length) * 100;
+
+        print("Star %: " + starPercentage);
+
+        if(starPercentage <= 34){
+            print("1 star!!!!!");
+        } else if(starPercentage <= 67){
+            print("2 stars!!!!!");
+        } else if(starPercentage <= 100){
+            print("3 stars yayyyy!!!!!");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("star")){
+            starsCollected++;
+            Destroy(other.gameObject);
+        }
     }
 }
