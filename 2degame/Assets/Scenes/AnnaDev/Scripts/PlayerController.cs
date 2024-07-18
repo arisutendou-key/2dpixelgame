434a8db75629c5dcd47fddb42653b6e0a8abed43
janjan1512 <janicevivienlau1512@gmail.com>
2024-07-18 10:43:32 -0400

    Update PlayerController.cs

--- 2degame/Assets/Scenes/AnnaDev/Scripts/PlayerController.cs
+++ 2degame/Assets/Scenes/AnnaDev/Scripts/PlayerController.cs
@@ -49,23 +49,7 @@ public class PlayerController : MonoBehaviour
         // setting all the velocity to the rigidbody
         rb2d.velocity = new Vector2(nextVelocityX, nextVelocityY);
 
-        print("All stars: " + totalStars.Length);
-        print("Stars collected: " + starsCollected);
-        starPercentage = (starsCollected / totalStars.Length) * 100;
-
-        print("Star %: " + starPercentage);
-
-        if(starPercentage <= 34){
-            print("1 star!!!!!");
-        } else if(starPercentage <= 67){
-            print("2 stars!!!!!");
-        } else if(starPercentage <= 100){
-            print("3 stars yayyyy!!!!!");
-        }
-
-        for(int i = 0; i < 10; i++){
-            Instantiate(stars, Vector2(Ran))
-        }
+        
     }
 
     void OnTriggerEnter2D(Collider2D other)
